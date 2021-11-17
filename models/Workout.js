7a7a1e885a6076bdf9cba0a7e679e79cd9ff60a2
dimitermusic <dimiteryyordanov@gmail.com>
2021-11-16 19:42:53 -0800

    models

---
+++ models/Workout.js
@@ -0,0 +1,46 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+// Create model to use for adding data to the Mongo database
+const WorkoutSchema = new Schema({
+  day: {
+    type: Number,
+  },
+  exercises: [
+    {
+      type: {
+        type: String,
+        trim: true,
+      },
+      name: {
+        type: String,
+        trim: true,
+      },
+
+      duration: {
+        type: Number,
+      },
+      distance: {
+        type: Number,
+      },
+      weight: {
+        type: Number,
+      },
+      reps: {
+        type: Number,
+      },
+      sets: {
+        type: Number,
+      },
+    },
+  ],
+  totalDuration: {
+    type: Number,
+    default: 0
+  }
+});
+
+const Workout = mongoose.model("Workout", WorkoutSchema);
+
+module.exports = Workout;
