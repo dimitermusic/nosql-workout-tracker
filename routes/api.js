908afb7f7998d2cfd1388ce8718581faaa6acd8a
dimitermusic <dimiteryyordanov@gmail.com>
2021-11-19 11:39:40 -0800

    fixed db issues

--- routes/api.js
+++ routes/api.js
@@ -1,5 +1,5 @@
 const router = require("express").Router();
-const { Workout } = require("../models");
+const Workout = require("../models/Workout.js");
 
 
 router.get("/api/workouts", (req, res) => {
